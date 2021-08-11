b2fb09e7997a1527f2de254ae7b43f4c5cdece7d
Abe Lincoln <--list>
2021-08-11 10:32:56 +0200

    home task 1

---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+let js ='amazing';
+console.log(40+80+23-10);
+console.log ('Jonas');
+let firstName ='Jonas';
+console.log(firstName);
+
+firstName = 'blabla';
+console.log(firstName);
+
+let javaScript = true;
+console.log(javaScript);
+
+console.log(typeof true);
+console.log(typeof javaScript);
+console.log(typeof 'boolean');
+
+javaScript = 23;
+console.log(typeof javaScript);
+
+let age = 30;
+age = 31;
+
+const birthYear = 1993;
+
+
+const ageEugen = 2021-1993;
+console.log (ageEugen);
+
+const now = 2021;
+const ageJonas = now - 1993;
+const ageSarah = now - 2010;
+
+console.log (ageJonas / 2, ageSarah *3);
+
+let x = 10 + 6;
+x += 10;
+console.log(x);
+
+console.log (ageSarah >= 18);
+
+let arr = [1, 2, 4, 5, 'bro', 'dope', 27];
+console.log(arr);
+console.log(typeof(arr));
+
+
+/*
+const firstName = 'Eugen'
+const job = 'QA'
+const birthYear = 1993;
+const year = 2021;
+
+const eugen = "I'm " + firstName +', a ' + (year - birthYear) + 'years old' + job + '!';
+console.log (eugen);
+
+const eugenNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+console.log (eugenNew);
+*/
