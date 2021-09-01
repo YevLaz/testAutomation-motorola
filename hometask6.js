d822c677d44ac475ae32113e3286025231037154
Abe Lincoln <--list>
2021-09-01 11:54:04 +0200

    HT Promise

--- hometask6.js
+++ hometask6.js
@@ -1,15 +1,16 @@
 // let ms = 0;
 
 
-async function delay (ms) {
+function delay (ms) {
 
-    let delayS = await new Promise ((res, rej) => {
+        return new Promise ((res, rej) => {
         setTimeout(() => {
+
             res()
         }, ms);
 
     })
-
 }
+
 
 delay(3000).then(() => alert('Wykonalo sie po 3 sekundach'));
